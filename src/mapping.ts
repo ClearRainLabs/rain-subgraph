9b83255a096fd0d0db904c2c2d8b54a1dd657965
Sam <hatem@clearrain.xyz>
2020-06-09 18:28:28 -0400

    handles new communities

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,71 +1,88 @@
-import { BigInt } from "@graphprotocol/graph-ts"
 import {
   Contract,
   NewCommunity,
   RoleGranted,
   RoleRevoked,
   SetCommunityTemplate
-} from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+} from "../generated/RainCommunity/Contract"
+import { Community, User } from "../generated/schema"
+import { Address, crypto, ByteArray, log } from '@graphprotocol/graph-ts'
+
+const OWNER_ROLE = 0xb19546dff01e856fb3f010c267a7b1c60363cf8a4664e21cc89c26224620214e
+const ADMIN_ROLE = 0xa49807205ce4d355092ef5a8a18f56e8913cf4a201fbe287825b095693c21775
+const MODERATOR_ROLE = 0x71f3d55856e4058ed06ee057d79ada615f65cdf5f9ee88181b914225088f834f
 
 export function handleNewCommunity(event: NewCommunity): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  log.info("HANDLING A NEW COMMUNITY: {}", [event.params.newCommunityAddress.toHex()])
+  let parentAddress = event.address
+  let parentCommunity = Community.load(parentAddress.toHex())
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  if (parentCommunity == null) {
+    parentCommunity = initCommunity(parentAddress)
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  let childCommunity = processChildCommunity(event, parentCommunity as Community)
 
-  // Entity fields can be set based on event parameters
-  entity.creator = event.params.creator
-  entity.newCommunityAddress = event.params.newCommunityAddress
+  let children = parentCommunity.childCommunities
+  children.push(childCommunity.id)
+  parentCommunity.childCommunities = children
 
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.ADMIN_ROLE(...)
-  // - contract.DEFAULT_ADMIN_ROLE(...)
-  // - contract.MODERATOR_ROLE(...)
-  // - contract.OWNER_ROLE(...)
-  // - contract.canAppend(...)
-  // - contract.childCommunities(...)
-  // - contract.communityTemplate(...)
-  // - contract.getRoleAdmin(...)
-  // - contract.getRoleMember(...)
-  // - contract.getRoleMemberCount(...)
-  // - contract.hasRole(...)
-  // - contract.isOpen(...)
-  // - contract.owner(...)
-  // - contract.parentCommunity(...)
-  // - contract.name(...)
-  // - contract.symbol(...)
+  parentCommunity.save()
 }
 
-export function handleRoleGranted(event: RoleGranted): void {}
+function initCommunity(address: Address): Community {
+  let com = new Community(address.toHex())
+  let contract = Contract.bind(address)
+
+  com.name = contract.name()
+  com.symbol = contract.symbol()
+  com.address = address
+  com.isOpen = contract.isOpen()
+
+  let ownerAddress = contract.owner() as Address
+  let comOwner = processUser(ownerAddress) as User
+
+  return com
+}
+
+function processChildCommunity(event: NewCommunity, parent: Community): Community {
+  log.info("PROCESSING CHILD COMMUNITY: {}, \nParent Community: {}", [
+    event.params.newCommunityAddress.toHex(),
+    parent.id
+  ])
+  let childAddress = event.params.newCommunityAddress.toHex()
+  let childContract = Contract.bind(event.params.newCommunityAddress)
+  let childCommunity = new Community(childAddress)
+
+  childCommunity.name = childContract.name()
+  childCommunity.symbol = childContract.symbol()
+  childCommunity.address = event.params.newCommunityAddress
+  childCommunity.parentCommunity = parent.id
+  childCommunity.childCommunities = []
+  childCommunity.isOpen = childContract.isOpen()
+
+  childCommunity.save()
+  return childCommunity
+}
+
+function processUser(address: Address): User {
+  let user = User.load(address.toHex())
+  if (user == null) {
+    user = new User(address.toHex())
+  }
+  user.address = address
+  user.save()
+  return user as User
+}
+
+export function handleRoleGranted(event: RoleGranted): void {
+  log.info("Role that was granted: {}", [event.params.role.toHex()])
+  let string = "OWNER_ROLE"
+  let arr = ByteArray.fromHexString(string)
+  arr = crypto.keccak256(arr)
+  log.info("Our arr of the string: {}", [arr.toHexString()])
+}
 
 export function handleRoleRevoked(event: RoleRevoked): void {}
 

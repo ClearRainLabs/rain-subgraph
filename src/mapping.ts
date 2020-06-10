304c8ab6bbdd6c37885cc2d1250a4dda21373ee7
Sam <hatem@clearrain.xyz>
2020-06-10 11:15:04 -0400

    feat: added suppport for member added/removed events

--- src/mapping.ts
+++ src/mapping.ts
@@ -3,14 +3,16 @@ import {
   NewCommunity,
   RoleGranted,
   RoleRevoked,
+  MemberAdded,
+  MemberRemoved,
   SetCommunityTemplate
 } from "../generated/RainCommunity/Contract"
 import { Community, User } from "../generated/schema"
 import { Address, crypto, ByteArray, log } from '@graphprotocol/graph-ts'
 
-const OWNER_ROLE = 0xb19546dff01e856fb3f010c267a7b1c60363cf8a4664e21cc89c26224620214e
-const ADMIN_ROLE = 0xa49807205ce4d355092ef5a8a18f56e8913cf4a201fbe287825b095693c21775
-const MODERATOR_ROLE = 0x71f3d55856e4058ed06ee057d79ada615f65cdf5f9ee88181b914225088f834f
+const OWNER_ROLE = '0xb19546dff01e856fb3f010c267a7b1c60363cf8a4664e21cc89c26224620214e'
+const ADMIN_ROLE = '0xa49807205ce4d355092ef5a8a18f56e8913cf4a201fbe287825b095693c21775'
+const MODERATOR_ROLE = '0x71f3d55856e4058ed06ee057d79ada615f65cdf5f9ee88181b914225088f834f'
 
 export function handleNewCommunity(event: NewCommunity): void {
   log.info("HANDLING A NEW COMMUNITY: {}", [event.params.newCommunityAddress.toHex()])
@@ -19,7 +21,6 @@ export function handleNewCommunity(event: NewCommunity): void {
 
   if (parentCommunity == null) {
     parentCommunity = initCommunity(parentAddress)
-
   }
 
   let childCommunity = processChildCommunity(event, parentCommunity as Community)
@@ -39,9 +40,6 @@ function initCommunity(address: Address): Community {
   com.symbol = contract.symbol()
   com.address = address
   com.isOpen = contract.isOpen()
-
-  let ownerAddress = contract.owner() as Address
-  let comOwner = processUser(ownerAddress) as User
 
   return com
 }
@@ -66,24 +64,50 @@ function processChildCommunity(event: NewCommunity, parent: Community): Communit
   return childCommunity
 }
 
-function processUser(address: Address): User {
+function processUser(address: Address): String {
   let user = User.load(address.toHex())
   if (user == null) {
     user = new User(address.toHex())
+    user.address = address
+    user.save()
   }
-  user.address = address
-  user.save()
-  return user as User
+  return user.id
 }
 
 export function handleRoleGranted(event: RoleGranted): void {
   log.info("Role that was granted: {}", [event.params.role.toHex()])
-  let string = "OWNER_ROLE"
-  let arr = ByteArray.fromHexString(string)
-  arr = crypto.keccak256(arr)
-  log.info("Our arr of the string: {}", [arr.toHexString()])
+
+  let address = event.address
+  let community = Community.load(address.toHex())
+
+  if (community == null) {
+    community = initCommunity(address)
+  }
+
+  let role = event.params.role.toHex()
+  let account = processUser(event.params.account)
+
+  switch (role) {
+    case OWNER_ROLE:
+      community.owner = user
+      break
+    case ADMIN_ROLE:
+      let admins = community.admins
+      admins.push(account)
+      community.admins = admins
+    case MODERATOR_ROLE:
+      let mods = community.moderators
+      mods.push(account)
+      community.moderators = mods
+  }
+
+  community.save()
 }
 
 export function handleRoleRevoked(event: RoleRevoked): void {}
+
+export function handleMemberAdded(event: MemberAdded): void {}
+
+export function handleMemberRemoved(event: MemberRemoved): void {}
 
 export function handleSetCommunityTemplate(event: SetCommunityTemplate): void {}
